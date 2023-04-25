031c9efee54c80ebd0eb1bb7d4193583777cfae9
Msamuelsons <marccossamuel@hotmail.com>
2023-04-25 02:34:30 +0000

    Em estudo

---
+++ aulasJS/aulaObjeto2.js
@@ -0,0 +1,16 @@
+let registro = {
+    nome: "Marcos Samuel",
+    idade: 22,
+    sexo: 'Masculino',
+
+    maisInformacoes: {
+        temCarteira: true,
+        casado: false,
+        faculdade: 'Faculdade Impacta',
+        carteira:['dinheiro', 'moeda', 'cartão de crédito']
+    }
+
+}
+
+console.log(registro)
+console.log(registro['maisInformacoes'])
